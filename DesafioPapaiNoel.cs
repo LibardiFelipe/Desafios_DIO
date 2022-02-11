5b2c1ec84dc46605fa6b06cde8995810b1229044
Felipe Libardi <53356038+LibardiFelipe@users.noreply.github.com>
2022-02-11 07:31:38 -0300

    Update DesafioPapaiNoel.cs

--- DesafioPapaiNoel.cs
+++ DesafioPapaiNoel.cs
@@ -1,3 +1,10 @@
+/*
+Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal.
+A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel.
+Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+*/
+
 using System;
 
 namespace DIO {
